c4f4b58988a516e3ceeade42626d04216d7ef685
Andrew0209 <3760209@gmail.com>
2024-08-25 21:28:56 +0300

    separate class

--- vec3.cpp
+++ vec3.cpp
@@ -1,1 +1,36 @@
 #include "vec3.h"
+#include <cmath>
+
+vec3::vec3(double _x, double _y, double _z) :x(_x), y(_y), z(_z) {}
+double x, y, z;
+
+double vec3::module() {
+	return sqrt(x * x + y * y + z * z);
+}
+
+void vec3::normalize() {
+	double mod = module();
+	x /= mod;
+	y /= mod;
+	z /= mod;
+}
+
+vec3 vec3::operator +(vec3 a) {
+	return { x + a.x, y + a.y, z + a.z };
+}
+
+vec3 vec3::operator -(vec3 a) {
+	return { x - a.x, y - a.y, z - a.z };
+}
+
+vec3 vec3::operator *(double n) {
+	return { x * n, y * n, z * n };
+}
+
+bool vec3::operator ==(vec3 a) {
+	return (a.x == x && a.y == y && a.z == z);
+}
+
+bool vec3::operator !=(vec3 a) {
+	return !(a.x == x && a.y == y && a.z == z);
+}
--- vec3.h
+++ vec3.h
@@ -6,29 +6,12 @@ class vec3
 public:
 	vec3(double _x, double _y, double _z):x(_x), y(_y), z(_z) {}
 	double x, y, z;
-	double module() {
-		return sqrt(x * x + y * y + z * z);
-	}
-	void normalize() {
-		double mod = module();
-		x /= mod;
-		y /= mod;
-		z /= mod;
-	}
-	vec3 operator +(vec3 a) {
-		return { x + a.x, y + a.y, z + a.z };
-	}
-	vec3 operator -(vec3 a) {
-		return { x - a.x, y - a.y, z - a.z };
-	}
-	vec3 operator *(double n) {
-		return { x * n, y * n, z * n };
-	}	
-	bool operator ==(vec3 a) {
-		return (a.x == x && a.y == y && a.z == z);
-	}
-	bool operator !=(vec3 a) {
-		return !(a.x == x && a.y == y && a.z == z);
-	}
+	double module();
+	void normalize();
+	vec3 operator +(vec3 a);
+	vec3 operator -(vec3 a);
+	vec3 operator *(double n);
+	bool operator ==(vec3 a);
+	bool operator !=(vec3 a);
 };
 
